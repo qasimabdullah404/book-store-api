2be118c13c6957718d4dd803b069fe49206c175e
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-02-19 22:14:39 +0500

    remove arg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
-  def not_destroyed(err)
+  def not_destroyed
     render json: {error: "Sorry! Record couldn't be destroyed, try again!"}, status: :unprocessable_entity
   end
 
