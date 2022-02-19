d4ad1916be78cfb604aacfa56c091872a6bf05f5
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-02-19 20:28:16 +0500

    add create action and update route

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.error, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:name, :author)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'books#index'
   namespace :api do
     namespace :v1 do
-      resources :books, only: :index
+      resources :books, only: [:index, :create]
     end
   end
 end
